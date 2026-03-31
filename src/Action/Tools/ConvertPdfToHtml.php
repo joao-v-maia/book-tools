1c6493af53fd1a5bde3c80603e4ddc32cf38ea03
João Vitor Maia <joao@dataclick.com.br>
2026-03-31 01:00:00 -0300

    fix: missing import in controller

--- src/Action/Tools/ConvertPdfToHtml.php
+++ src/Action/Tools/ConvertPdfToHtml.php
@@ -6,6 +6,7 @@ use App\Service\Pdf\PdfConverter;
 use App\Service\Pdf\PdfMerger;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
